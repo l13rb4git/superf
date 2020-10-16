d5390d2dc3081ed422660cc9121d34cce157ad42
macunaima <gcgamer46@gmail.com>
2020-10-16 14:32:24 -0300

    inital show page created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
 
   def index
     
   end
 
+  def show
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
@@ -17,5 +22,9 @@ class PostsController < ApplicationController
     
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def find_post
+      @post = Post.find_by(params[:id])
     end
 end
