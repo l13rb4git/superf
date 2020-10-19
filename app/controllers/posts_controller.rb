ba5345e781d08211e78fe31c17efac7239606749
macunaima <gcgamer46@gmail.com>
2020-10-19 14:29:51 -0300

    Added post editing fuctionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -20,6 +20,22 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    
   end
 
   private
