8d00059916c2c6b304b2ee82b59719ad5a32e703
macunaima <gcgamer46@gmail.com>
2020-10-19 14:06:11 -0300

    showing which errors the new post has

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,12 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
