8b6a6693f1cfeaa916cc69c214310d07d55cbc25
macunaima <gcgamer46@gmail.com>
2020-10-20 10:06:15 -0300

    before_action to find posts by id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 class CommentsController < ApplicationController
+  before_action :find_post
 
   def create
-    @post = Post.find_by(id: params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
 
     redirect_to post_path(@post)
   end
+
+  private
+
+    def find_post
+      @post = Post.find_by(id: params[:id])
+    end
 end
